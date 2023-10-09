7d9bb6c1b448ac5eb0474c3fb902b82e8f758654
niciima <naemamahmaued@gmail.com>
2023-10-09 05:50:06 -0700

    hello

---
+++ multibalicationnnn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multibalicationnumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("enter any number");
+            i = int.Parse(Console.ReadLine());
+           
+            do
+            {
+                int secoundpower = i * i;
+                Console.WriteLine(i + "x" + i + "=" + secoundpower);
+                i--;
+
+
+
+            }
+            while (i > 0);
+            Console.ReadLine();
+
+        }
+    }
+}
